4de7f8ae663099c9e530bf58ccfab2b2dee6ffde
Opala, Artur <artur.opala_external@immobilienscout24.de>
2014-11-24 16:22:56 +0100

    extracted HttpService

--- src/main/scala/trafficlightscontrol/Boot.scala
+++ src/main/scala/trafficlightscontrol/Boot.scala
@@ -1,16 +1,11 @@
 package trafficlightscontrol
 
-import akka.actor.{ ActorSystem, Props, ActorRef }
+import scala.concurrent.duration.DurationInt
+
+import akka.actor.ActorSystem
 import akka.io.IO
-import spray.can.Http
-import akka.pattern.ask
 import akka.util.Timeout
-import scala.concurrent.duration._
-import akka.actor.Actor
-import spray.routing._
-import spray.http._
-import MediaTypes._
-import akka.actor.ActorLogging
+import spray.can.Http
 
 object Boot extends App {
 
@@ -18,84 +13,5 @@ object Boot extends App {
   implicit val module = new Module {}
 
   implicit val timeout = Timeout(5.seconds)
-  IO(Http) ? Http.Bind(module.httpServiceActor, interface = "0.0.0.0", port = 8080)
-}
-
-class HttpServiceActor(monitoring: ActorRef) extends Actor with TrafficHttpService {
-  def actorRefFactory = context
-  val route = createRoutes(monitoring)
-  def receive = runRoute(route)
-}
-
-trait TrafficHttpService extends HttpService {
-
-  import Directives._
-
-  object GetReportQueryTimeout
-
-  val exceptionHandler = {
-    ExceptionHandler {
-      case e: Throwable =>
-        complete(StatusCodes.InternalServerError)
-    }
-  }
-
-  val rejectionHandler = {
-    RejectionHandler {
-      case MissingQueryParamRejection(param) :: _ =>
-        complete(StatusCodes.BadRequest, s"Request is missing required query parameter '$param'")
-    }
-  }
-
-  def createRoutes(monitoring: ActorRef) =
-    handleRejections(rejectionHandler) {
-      handleExceptions(exceptionHandler) {
-        path("status") {
-          get {
-            streamStatusResponse(monitoring)
-          }
-        } ~
-          pathPrefix("") {
-            getFromResourceDirectory("")
-          } ~
-          path("") {
-            getFromResource("index.html")
-          }
-      }
-    }
-
-  def streamStatusResponse(monitoring: ActorRef)(ctx: RequestContext): Unit =
-    actorRefFactory.actorOf {
-      Props {
-        new Actor with ActorLogging {
-
-          val responseStart = HttpResponse(entity = HttpEntity(`application/json`, "{\"status\":{"))
-          ctx.responder ! ChunkedResponseStart(responseStart)
-
-          def receive = {
-            case ReportEvent(report) => {
-              timeoutTask.cancel()
-              ctx.responder ! MessageChunk(report map { case (k, v) => s""""$k":"$v"""" } mkString ("", ",", "}}"))
-              ctx.responder ! ChunkedMessageEnd
-              context.stop(self)
-            }
-            case GetReportQueryTimeout => {
-              ctx.responder ! MessageChunk("}}")
-              ctx.responder ! ChunkedMessageEnd
-              context.stop(self)
-            }
-            case ev: Http.ConnectionClosed => {
-              timeoutTask.cancel()
-              log.warning("Stopping response streaming due to {}", ev)
-            }
-
-          }
-
-          val timeoutTask = context.system.scheduler.scheduleOnce(1.seconds, self, GetReportQueryTimeout)(context.system.dispatcher)
-
-          monitoring ! GetReportQuery
-
-        }
-      }
-    }
+  IO(Http) ! Http.Bind(module.httpServiceActor, interface = "0.0.0.0", port = 8080)
 }
---
+++ src/main/scala/trafficlightscontrol/HttpService.scala
@@ -0,0 +1,90 @@
+package trafficlightscontrol
+
+import akka.actor.{ Actor, ActorSystem, Props, ActorRef, ActorLogging }
+import akka.io.IO
+import spray.can.Http
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+class HttpServiceActor(monitoring: ActorRef) extends Actor with TrafficHttpService {
+  def actorRefFactory = context
+  val route = createRoutes(monitoring)
+  def receive = runRoute(route)
+}
+
+trait TrafficHttpService extends HttpService {
+
+  import Directives._
+
+  object GetReportQueryTimeout
+
+  val exceptionHandler = {
+    ExceptionHandler {
+      case e: Throwable =>
+        complete(StatusCodes.InternalServerError)
+    }
+  }
+
+  val rejectionHandler = {
+    RejectionHandler {
+      case MissingQueryParamRejection(param) :: _ =>
+        complete(StatusCodes.BadRequest, s"Request is missing required query parameter '$param'")
+    }
+  }
+
+  def createRoutes(monitoring: ActorRef) =
+    handleRejections(rejectionHandler) {
+      handleExceptions(exceptionHandler) {
+        path("status") {
+          get {
+            streamStatusResponse(monitoring)
+          }
+        } ~
+          pathPrefix("") {
+            getFromResourceDirectory("")
+          } ~
+          path("") {
+            getFromResource("index.html")
+          }
+      }
+    }
+
+  def streamStatusResponse(monitoring: ActorRef)(ctx: RequestContext): Unit =
+    actorRefFactory.actorOf {
+      Props {
+        new Actor with ActorLogging {
+
+          val responseStart = HttpResponse(entity = HttpEntity(`application/json`, "{\"status\":{"))
+          ctx.responder ! ChunkedResponseStart(responseStart)
+
+          def receive = {
+            case ReportEvent(report) => {
+              timeoutTask.cancel()
+              ctx.responder ! MessageChunk(report map { case (k, v) => s""""$k":"$v"""" } mkString ("", ",", "}}"))
+              ctx.responder ! ChunkedMessageEnd
+              context.stop(self)
+            }
+            case GetReportQueryTimeout => {
+              ctx.responder ! MessageChunk("}}")
+              ctx.responder ! ChunkedMessageEnd
+              context.stop(self)
+            }
+            case ev: Http.ConnectionClosed => {
+              timeoutTask.cancel()
+              log.warning("Stopping response streaming due to {}", ev)
+            }
+
+          }
+
+          val timeoutTask = context.system.scheduler.scheduleOnce(1.seconds, self, GetReportQueryTimeout)(context.system.dispatcher)
+
+          monitoring ! GetReportQuery
+
+        }
+      }
+    }
+}
