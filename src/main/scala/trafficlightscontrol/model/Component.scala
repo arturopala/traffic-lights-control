6a719cae258df932a2b93ea3619765c32a02520c
Artur Opala <artur.opala@siili.fi>
2015-10-16 12:47:12 +0200

    reformat Component definitions

--- src/main/scala/trafficlightscontrol/model/Component.scala
+++ src/main/scala/trafficlightscontrol/model/Component.scala
@@ -6,10 +6,38 @@ sealed trait Component { def id: Id; def configuration: Configuration }
 sealed trait CompositeComponent extends Component { def members: Iterable[Component] }
 sealed trait SingleMemberComponent extends CompositeComponent { def member: Component; def members: Iterable[Component] = Seq(member) }
 
-case class Light(id: Id, initialState: LightState = RedLight)(implicit val configuration: Configuration) extends Component
-case class Sequence(id: Id, strategy: SequenceStrategy, members: Component*)(implicit val configuration: Configuration) extends CompositeComponent
-case class Group(id: Id, members: Component*)(implicit val configuration: Configuration) extends CompositeComponent
-case class Switch(id: Id, member: Component, initiallyGreen: Boolean = false, skipTicks: Int = 0)(implicit val configuration: Configuration) extends SingleMemberComponent
-case class Pulse(id: Id, member: Component, skipTicks: Int = 0)(implicit val configuration: Configuration) extends SingleMemberComponent
-case class Offset(id: Id, offset: FiniteDuration, member: Component)(implicit val configuration: Configuration) extends SingleMemberComponent
+case class Light(
+  id:           Id,
+  initialState: LightState = RedLight
+)(implicit val configuration: Configuration) extends Component
+
+case class Sequence(
+  id:       Id,
+  strategy: SequenceStrategy,
+  members:  Component*
+)(implicit val configuration: Configuration) extends CompositeComponent
+
+case class Group(
+  id:      Id,
+  members: Component*
+)(implicit val configuration: Configuration) extends CompositeComponent
+
+case class Switch(
+  id:             Id,
+  member:         Component,
+  initiallyGreen: Boolean   = false,
+  skipTicks:      Int       = 0
+)(implicit val configuration: Configuration) extends SingleMemberComponent
+
+case class Pulse(
+  id:        Id,
+  member:    Component,
+  skipTicks: Int       = 0
+)(implicit val configuration: Configuration) extends SingleMemberComponent
+
+case class Offset(
+  id:     Id,
+  offset: FiniteDuration,
+  member: Component
+)(implicit val configuration: Configuration) extends SingleMemberComponent
 
