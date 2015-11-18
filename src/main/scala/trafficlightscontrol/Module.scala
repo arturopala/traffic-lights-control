3d72133e1f1c8c3e816a35d7b5772db74aa9cc79
Artur Opala <artur.opala@siili.fi>
2015-11-18 10:19:34 +0100

    upgrade macwire dependency

--- src/main/scala/trafficlightscontrol/Module.scala
+++ src/main/scala/trafficlightscontrol/Module.scala
@@ -2,7 +2,7 @@ package trafficlightscontrol
 
 import scala.reflect.ClassTag
 
-import com.softwaremill.macwire.Macwire
+import com.softwaremill.macwire._
 import akka.actor.Props
 import scala.concurrent.duration._
 import akka.actor.IndirectActorProducer
@@ -17,15 +17,15 @@ import akka.http.scaladsl.Http
 
 import trafficlightscontrol.actors._, trafficlightscontrol.http._
 
-class Module(implicit system: ActorSystem, materializer: ActorMaterializer) extends Macwire with ActorOf {
+class Module(implicit system: ActorSystem, materializer: ActorMaterializer) {
 
   val interval: FiniteDuration = 5.seconds
   lazy val initialLayouts = DemoLayouts.layouts
-  lazy val manager: TrafficSystemsManager = TrafficSystemsManager(actorOf[TrafficSystemsManagerActor]("manager", initialLayouts))
+  lazy val manager: TrafficSystemsManager = TrafficSystemsManager(ActorOf[TrafficSystemsManagerActor]("manager", initialLayouts))
   lazy val monitoring: Monitoring = Monitoring(system.actorOf(Props(classOf[MonitoringActor]).withDispatcher("monitoring-pinned-dispatcher"), "monitoring"))
   lazy val httpService: HttpService = wire[HttpService]
 }
 
-trait ActorOf {
-  def actorOf[T](name: String, args: Any*)(implicit factory: ActorRefFactory, ct: ClassTag[T]): ActorRef = factory.actorOf(Props(ct.runtimeClass, args: _*), name)
+object ActorOf {
+  def apply[T](name: String, args: Any*)(implicit factory: ActorRefFactory, ct: ClassTag[T]): ActorRef = factory.actorOf(Props(ct.runtimeClass, args: _*), name)
 }
